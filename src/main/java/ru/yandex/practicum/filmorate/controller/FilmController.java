080ac36e1eb688ea19e5eadcc603ed032f530d57
LeatherBastard <kostrykinmark@yandex.ru>
2023-08-11 11:24:10 +0300

    Add REST controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,10 +1,40 @@
 package ru.yandex.practicum.filmorate.controller;
 
+
+import org.springframework.web.bind.annotation.*;
+
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 
+
+
+@RestController
+@RequestMapping("/films")
 public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
+    private static int id = 0;
+
+    @GetMapping
+    public List<Film> getFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        film = new Film(getId(), film.getName(), film.getDescription(), film.getReleaseDate(), film.getDuration());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    private static int getId() {
+        return ++id;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,10 +1,41 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
     private final Map<Integer, User> users = new HashMap<>();
+    private static int id = 0;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        user = new User(getId(), user.getEmail(), user.getLogin(), user.getName(), user.getBirthday());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private static int getId() {
+        return ++id;
+    }
 }
