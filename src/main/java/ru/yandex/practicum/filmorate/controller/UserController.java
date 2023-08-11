35e7cd54c146096d5581c6845da2416cfa6ee358
LeatherBastard <kostrykinmark@yandex.ru>
2023-08-11 11:11:40 +0300

    Add storage fields for controllers

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+}
